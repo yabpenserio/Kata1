fe30d78410014bb9c6bb35dfa23386181f958cbd
yabpe <yabpe@10.193.38.11>
2022-09-15 12:44:37 +0100

    Segundo Commit

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,18 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Sara", new Date(102,7,6));
-        System.out.println(person.getName() + " tiene " + person.getAge() + " años");
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(2002,7,6);
+        
+        Person person = new Person("Sara", date);
+        System.out.println(person.getName() + " tiene " + person.getAge() 
+                + " años");
     }
     
 }
--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -1,12 +1,14 @@
 package kata1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
     private final String name;
-    private final Date birthdate;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
     
-    public Person(String name, Date birthdate){
+    public Person(String name, Calendar birthdate){
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -15,10 +17,15 @@ public class Person {
         return name;
     }
 
-    public Date getBirthdate() {
+    public Calendar getBirthdate() {
         return birthdate;
     }
     public int getAge() {
-        return (int) ((new Date().getTime()- birthdate.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() -
+                birthdate.getTimeInMillis()));
+    }
+    private long milliSecondsToYear(long milles){
+        return milles / MILLISECONDS_PER_YEAR;
     }
 }
